962483def9d997c41569c8e6762707a95e14fde5
Ehsan Asdar <ehsanmasdar@gmail.com>
2016-09-10 15:36:53 -0400

    Push major update to UI and algo

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,29 @@
-console.log('\'Allo \'Allo! Popup');
+setInterval(function () {
+  chrome.storage.sync.get('live', function(items){
+    document.getElementById("current-speed").innerHTML =  items.live;
+  });
+  chrome.storage.sync.get('avg', function(items){
+    if (items.avg.length > 0){
+      var avg = 0;
+      for (var i = 0; i< items.avg.length; i++){
+        avg += items.avg[i];
+      }
+      avg /= items.avg.length;
+      document.getElementById("average-speed").innerHTML =  avg;
+    }
+    else {
+      document.getElementById("average-speed").innerHTML = 0;
+    }
+  });
+    
+},500);
 
 
-chrome.storage.sync.get('avg', function(items){
-    console.log(items);
- });
+document.addEventListener('DOMContentLoaded', function() {
+    var reset = document.getElementById('reset');
+    // onClick's logic below:
+    reset.addEventListener('click', function() {
+      chrome.storage.sync.set({'avg':[]}, function(){})
+      document.getElementById("average-speed").innerHTML = 0;
+    });
+});
