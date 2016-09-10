93742d6c065012661b5ad5245607f3753b6db254
Jenny Ma <jy4ma@uwaterloo.ca>
2016-09-10 19:30:10 -0400

    UI Updates

---
+++ penn-apps-lab1-master/app/src/main/java/com/example/zachcosta/helloworld/DisplayMessageActivity.java
@@ -0,0 +1,27 @@
+package com.example.zachcosta.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /** Fill In Here */
+    }
+
+}
